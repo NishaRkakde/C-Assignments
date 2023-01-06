b4e85b0bdcdcb8fe4f030b64d2cec83fe16bd9a0
NIKAKADE <NIKAKADE@LIN24008241>
2023-01-06 17:25:00 +0530

    Assignment 7_1

---
+++ assignment7_1/assignment7_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace assignment7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create directory
+            String path = @"C:\Users\NIKAKADE\source\repos\assignment7_1";
+            DirectoryInfo f1 = new DirectoryInfo(path);
+
+            f1.Create();//to display msg
+            
+                Console.WriteLine("Directory Created");     
+            
+            Console.WriteLine("Directory path =" + f1.FullName);
+
+            //get the directory name
+            Console.WriteLine("Directory Name = " + f1.Name);
+
+            //get the parent directory
+            Console.WriteLine("Parent Diretcory = " + f1.Parent);
+            //Console.WriteLine( f1.LastAccessTime);
+            //Return name of sub directory
+            string[] directory = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            //return the name of sub file
+            string[] file = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            //retrun name of sub files
+            Console.WriteLine("Directory Details with sub directory--");
+
+            foreach(String diNext in directory)
+            {
+                var dirInfo = new DirectoryInfo(diNext);
+                Console.WriteLine(dirInfo.FullName);
+            }
+            foreach(String fiNext in directory)
+            {
+                var fileInfo = new FileInfo(fiNext);
+                Console.WriteLine(fileInfo.FullName);
+            }
+       }
+    }
+}
