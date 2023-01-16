b2ea99adf00b0e75a895d8fa1747c781f3527c06
NIKAKADE <NIKAKADE@LIN24008241>
2023-01-16 22:32:46 +0530

    commiting file

---
+++ Assignment_6_Q3/Assignment_6_Q3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account cust = new Account(9083456, "Devashri More", 500);
+
+            cust.Deposit(4000);
+            ZeroBalance z = new ZeroBalance(cust.ZeroBalance);
+            del ub = new del(cust.UnderBalance);
+            cust.Withdraw(400, ub, z);
+        }
+    }
+}
