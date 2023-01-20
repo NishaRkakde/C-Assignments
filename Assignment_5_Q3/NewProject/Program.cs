6f9f8e531b53886faf7b49832464b91aa9723c1a
NIKAKADE <NIKAKADE@LIN24008241>
2023-01-20 15:45:59 +0530

    Commiting assignment 5

---
+++ Assignment_5_Q3/NewProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewProject
+{
+    class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+         
+        public Employee(int empid, string empname)
+        {
+            this.EmpId = empid;
+            this.EmpName = empname;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Emplist = new List<Employee>();
+            Emplist.Add(new Employee(1, "Nisha"));
+            Emplist.Add(new Employee(2, "vijaya"));
+            foreach (Employee e in Emplist)
+            {
+                Console.WriteLine(e.EmpId + " " + e.EmpName);
+            }
+            Console.WriteLine(Emplist.Count);
+        }
+    }
+}
